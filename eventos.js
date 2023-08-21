006646da69095a3e569dd0d6de32bddb4470dbfb
lucassoy <lucascapouno@gmail.comy>
2023-08-21 00:28:08 -0300

    rama

--- eventos.js
+++ eventos.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(){
 var boton = document.getElementById("boton");
 
 function mensaje(){
     alert("Hola! soy el div");
 }
 boton.addEventListener("click",mensaje);
-});
